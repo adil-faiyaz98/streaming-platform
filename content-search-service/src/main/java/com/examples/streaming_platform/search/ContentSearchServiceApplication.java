f9a7f5ad1d974627cb56608182af46ee839746a0
Adil Faiyaz <adilmd98@gmail.com>
2025-03-08 05:16:50 -0500

    feat: add ContentSearchServiceApplication and NotificationServiceApplication classes

---
+++ content-search-service/src/main/java/com/examples/streaming_platform/search/ContentSearchServiceApplication.java
@@ -0,0 +1,12 @@
+package com.examples.streaming_platform.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentSearchServiceApplication.class, args);
+    }
+}
