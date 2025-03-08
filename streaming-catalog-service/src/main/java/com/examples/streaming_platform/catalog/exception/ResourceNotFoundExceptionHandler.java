157aaece5f8e49a3ccafde5c04cee7b7024aedc0
Adil Faiyaz <adilmd98@gmail.com>
2025-03-08 00:57:38 -0500

    refactor: simplify ResourceNotFoundException and remove handler class

--- streaming-catalog-service/src/main/java/com/examples/streaming_platform/catalog/exception/ResourceNotFoundExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-// exception/ResourceNotFoundException.java
-package com.examples.streaming_platform.catalog.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
