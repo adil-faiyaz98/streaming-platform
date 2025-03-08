6bc02f91b663759745379a82ee0b045acb4f931f
Adil Faiyaz <adilmd98@gmail.com>
2025-03-08 07:02:18 -0500

    feat: update build configuration to include MapStruct and H2 for testing; add new application classes for review, billing, and recommendation services

---
+++ subscription-billing-service/src/main/java/com/examples/streaming_platform/billing/SubscriptionBillingApplication.java
@@ -0,0 +1,11 @@
+package com.examples.streaming_platform.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionBillingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionBillingApplication.class, args);
+    }
+}
