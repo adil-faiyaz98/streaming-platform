97536445509d5dc7e2a84423431bf2e6301e5ecf
Adil Faiyaz <adilmd98@gmail.com>
2025-03-08 20:01:53 -0500

    feat: disable Eureka client in StreamingCatalogApplication and update application.yml

--- streaming-catalog-service/src/main/java/com/examples/streaming_platform/catalog/StreamingCatalogApplication.java
+++ streaming-catalog-service/src/main/java/com/examples/streaming_platform/catalog/StreamingCatalogApplication.java
@@ -2,10 +2,8 @@ package com.examples.streaming_platform.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class StreamingCatalogApplication {
     public static void main(String[] args) {
         SpringApplication.run(StreamingCatalogApplication.class, args);
