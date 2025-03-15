9ad6a635f838399fa29b415d2bf112b2616d9148
Adil Faiyaz <adilmd98@gmail.com>
2025-03-14 20:24:16 -0400

    feat: add CORS configuration for GraphQL and GraphiQL endpoints

---
+++ streaming-catalog-service/src/main/java/com/examples/streaming_platform/catalog/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.examples.streaming_platform.catalog.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/graphql")
+                .allowedOriginPatterns("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+        registry.addMapping("/graphiql")
+                .allowedOriginPatterns("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
